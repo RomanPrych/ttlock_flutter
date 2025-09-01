be0828d71cb81580f93f89c0e34a0f532b933cb5
romanpr <rm24052020@gmail.com>
2025-09-01 14:16:20 +0300

    ss

--- lib/ttlock.dart
+++ lib/ttlock.dart
@@ -2097,7 +2097,7 @@ typedef TTBluetoothStateCallback = void Function(TTBluetoothState state);
 typedef TTBluetoothScanStateCallback = void Function(bool isScanning);
 typedef TTLockDataCallback = void Function(String lockData);
 typedef TTControlLockCallback = void Function(
-    int lockTime, int electricQuantity, int? uniqueId, String? lockData);
+    int lockTime, int electricQuantity, int uniqueId, String lockData);
 typedef TTGetAdminPasscodeCallback = void Function(String adminPasscode);
 typedef TTGetLockElectricQuantityCallback = void Function(int electricQuantity);
 typedef TTGetLockOperateRecordCallback = void Function(String records);
